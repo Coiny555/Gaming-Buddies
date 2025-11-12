c53b0174f7357664859db5dd8dc0c85bf391e903
Kenyon <hg7959@wayne.edu>
2025-11-11 21:00:01 -0500

    Properly naming functions to reduce confusion

--- GUIfunctionality.js
+++ GUIfunctionality.js
@@ -2,11 +2,11 @@ const {app, BrowserWindow} = require("electron");
 
 let win = null;
 
-const createWindow = () => {
+const YourBioWindow = () => {
     win = new BrowserWindow({
         width: 800, //In pixels
         height: 600,
-        resizable: false, //Will change to true soon.
+        resizable: false, //Debating on if wanting to use true.
         webPreferences: {
             nodeIntegration: true //Have node functions and node API's
         }
@@ -15,7 +15,8 @@ const createWindow = () => {
     win.loadFile('YourBio.html'); //Load html file, put that in the quotation marks!
 };
 
-app.whenReady().then(createWindow);
+
+app.whenReady().then(YourBioWindow);
 
 
 // For users to edit their own bios.
