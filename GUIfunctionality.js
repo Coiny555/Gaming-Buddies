8361fddc48b19e1f860317a256e809794cd559e0
Kenyon <hg7959@wayne.edu>
2025-12-02 20:17:24 -0500

    Adding more to the GUI for looks

--- GUIfunctionality.js
+++ GUIfunctionality.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require("electron");
 
 let win = null;
 
-const YourBioWindow = () => {
+const YourBioWindow = () => { //Opens a GUI for User(you) bio.
     win = new BrowserWindow({
         width: 800, //In pixels
         height: 600,
@@ -15,8 +15,23 @@ const YourBioWindow = () => {
     win.loadFile('YourBio.html'); //Load html file, put that in the quotation marks!
 };
 
+const YourFriendWindow = () => { //Opens a GUI for User(you) bio.
+    win = new BrowserWindow({
+        width: 800, //In pixels
+        height: 600,
+        resizable: false, //Debating on if wanting to use true.
+        webPreferences: {
+            nodeIntegration: true //Have node functions and node API's
+        }
+    })
+
+    win.loadFile('FriendBio.html'); //Load html file, put that in the quotation marks!
+};
+
 
 app.whenReady().then(YourBioWindow);
+app.whenReady().then(YourFriendWindow);
+
 
 
 // For users to edit their own bios.
